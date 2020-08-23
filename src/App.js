e2be0382da46fe5aae98c765197f21bfad1291dc
Berk Hüseyin Gaffaroğlu <berkgaffaroglu@Berks-MacBook-Pro.local>
2020-08-23 04:08:42 +0300

    heroku

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import APOD from './components/APOD';
 import Navigation from './components/Navigation';
 import Curiosity from './components/rover_footages/Curiosity';
@@ -20,10 +20,10 @@ function App() {
         <div className='container content'>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="apod/" component={(props) => <APOD api_key={api_key} />} />
-            <Route path="roverfootage/curiosity/" component={(props) => <Curiosity api_key={api_key} />}/>
-            <Route path="roverfootage/opportunity/" component={(props) => <Opportunity api_key={api_key} />} />
-            <Route path="roverfootage/spirit/" component={(props) => <Spirit api_key={api_key} />} />
+            <Route path="/apod" component={(props) => <APOD api_key={api_key} />} />
+            <Route path="/roverfootage/curiosity" component={(props) => <Curiosity api_key={api_key} />}/>
+            <Route path="/roverfootage/opportunity" component={(props) => <Opportunity api_key={api_key} />} />
+            <Route path="roverfootage/spirit" component={(props) => <Spirit api_key={api_key} />} />
           </Switch>
 
         </div>
